05a539dc5e96e0c24bf4a0547a6a57c12c7242f8
Võ Thành Nguyễn <64852371+vtnguyen04@users.noreply.github.com>
2026-09-14 00:14:56 +0700

    Step 4: standardize_features

--- model.py
+++ model.py
@@ -61,8 +61,10 @@ def compute_feature_stats(X):
 
     return mean, std
 
-# Step 4 - standardize_features (not yet solved)
-# TODO: implement
+# Step 4 - standardize_features
+def standardize_features(X, mean, std):
+    # TODO: Apply z-score normalization using precomputed training mean and std.
+    return (X - mean) / std
 
 # Step 5 - add_bias_column (not yet solved)
 # TODO: implement
